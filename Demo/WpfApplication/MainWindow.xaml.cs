63fc20187ff3eec3583e19d02b26d408f1b41a06
FelixChen1 <felix.chen.zhuhai@quest.com>
2018-04-12 18:35:47 +0800

    Init solution

---
+++ Demo/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowDialog<DockPanelDialogView>();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ShowDialog<WrapPanelDialogView>();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ShowDialog<GridPanelDialogView>();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ShowDialog<MixDialogView>();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ShowDialog<UserControlDialogView>();
+        }
+
+        private static void ShowDialog<T>() where T : Window, new()
+        {
+            T dialog = new T();
+            dialog.ShowDialog();
+        }
+
+    }
+}
---
+++ Demo/WpfApplication/UserControlDialogView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for UserControlDialogView.xaml
+    /// </summary>
+    public partial class UserControlDialogView : Window
+    {
+        public UserControlDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
